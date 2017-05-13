c8e569639e6cb4af4232e2c6d0531af8ad3a7848
Steven Strybos <stevengstrybos@gmail.com>
2017-05-13 13:55:29 -0500

    Started parsing search result

--- assets/javascript/articleSearch.js
+++ assets/javascript/articleSearch.js
@@ -3,8 +3,13 @@
 
       $("#searchButton").on("click", function() {
       event.preventDefault();
-      var article;
-      var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api_key="+ api_key;
+      var searchInput = $("#searchInput").val();
+      console.log(searchInput);
+      var numRecs = $("#numberRecords").val();
+      console.log(numRecs);
+      var start = $("#startYearInput").val();
+      var end = $("#endYearInput").val();
+      var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api_key="+ api_key+"?q="+searchInput+"?page="+numRecs;
 
       $.ajax({
           url: queryURL,
@@ -13,7 +18,7 @@
         .done(function(response) {
           console.log(response);
           var results = response.docs;
-          var title = results[0].headline.main;
+          // var title = results[0].headline.main;
           var author = results[0].source;
           var section = results[0].subsection_name;
           var date = results[0].pub_date;
